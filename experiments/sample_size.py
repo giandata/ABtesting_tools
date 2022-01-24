b8dced587d87c2a2f293ff87aa73cc8e1cfeed96
giandata <happydaysbcn2017@gmail.com>
2022-01-24 20:39:15 +0100

    unequal sample size calc

--- experiments/sample_size.py
+++ experiments/sample_size.py
@@ -18,3 +18,26 @@ def sample_size_calc(alpha, beta, conversion_base, conversion_desired):
 
     return int(round((pool_variance / (c1-c2)**2) *
                      (z_table_beta[beta] + z_table_alpha[alpha])**2, 0))
+
+
+def unequal_samples(n, k):
+
+    j = 100-k
+    ratio = round((j / k), 2)
+    adjusted_sample = int(round((n * (1 + ratio)**2) / (ratio * 4), 0))
+    control_sample = int(round(adjusted_sample / (1+ratio), 0))
+    test_sample = int(round((adjusted_sample - control_sample), 0))
+
+    return (ratio, adjusted_sample, control_sample, test_sample)
+
+    # N = sample
+    # N1 = (n(1+k)**2 )/ 4*k
+    #control = N1/(1+k)
+    #variant = k*N1/(1+k)
+
+    # N = 1688 k= 0,9
+
+    # n1 = (1688*3.61)/7,6 = 802
+
+    # control = 802/1,9 = 422
+    # variant = 0,9* 802 /1,9 = 382
--- views/sample_size.py
+++ views/sample_size.py
@@ -19,24 +19,27 @@ def render():
                                    options=[90, 95, 98, 99], help="Establish the significance level")
     test_power = sample_form.radio(
         label='Test power %', options=[80, 90, 95], help="the probability of detecting that difference between the original rate and the variant conversion rates")
-    submit = sample_form.form_submit_button('Calculate')
 
-    if submit:
+    sample_form.subheader("Unequal sample size")
+
+    k = sample_form.slider(label="Control group allocation %",
+                           min_value=10, value=50, max_value=95, step=5)
+
+    submit_sizes = sample_form.form_submit_button('Calculate sample')
+    if submit_sizes:
 
         sample = sample_size.sample_size_calc(
             sign_level, test_power, baseline, mde)
 
-        st.subheader(f"Sample size per variant needed: {sample}")
+        st.subheader(f"Sample size needed at equal group size: {sample*2}")
         st.info(
-            f"To observe at  least a {mde} % improvement with a {sign_level} % significance levels and a {test_power} % probability, the experiment will need a sample of  {sample} for each variant")
+            f"To observe at  least a {mde} % improvement with a {sign_level} % significance levels and a {test_power} % probability, the experiment will need a sample of  {sample} observations for each group.")
 
-        # N1 = (n(1+k)**2 )/ 4*k
-        #control = N1/(1+k)
-        #variant = k*N1/(1+k)
+        (ratio, adjusted_sample, sample_control,
+            sample_test) = sample_size.unequal_samples(sample, k)
 
-        # N = 1688 k= 0,9
-
-        # n1 = (1688*3.61)/7,6 = 802
-
-        # control = 802/1,9 = 422
-        # variant = 0,9* 802 /1,9 = 382
+        st.subheader(
+            f"Adjusted sample with unequal groups size: {adjusted_sample}")
+        st.write(f"Control sample {sample_control} ")
+        st.write(f"Test sample {sample_test}")
+        st.write(f"Experiment group ratio {ratio} %")
